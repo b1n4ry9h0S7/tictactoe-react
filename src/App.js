682f200b338e92c495a0c9e3a34ee4a9732f6e9c
Manjunath Shenoy <mshenoy98@gmail.com>
2018-11-23 11:28:07 +0530

    Changed game state to state. I dont think its good idea to name it as gamestate

--- src/App.js
+++ src/App.js
@@ -11,64 +11,63 @@ class App extends Component {
     this.state = {
       winnerLine:'Let\'s play...!',
       winner: undefined,
+      turn: 'X',
+      gameLocked: false,
+      gameEnded: false,
+      board: Array(9).fill(''),
+      totalMoves: 0
     };
-    this.gameState = {
-        turn: 'X',
-        gameLocked: false,
-        gameEnded: false,
-        board: Array(9).fill(''),
-        totalMoves: 0
-    }
+
   }
 
   clicked = (box) => {
     
-    if(this.gameState.gameEnded || this.gameState.gameLocked) return;
+    if(this.state.gameEnded || this.state.gameLocked) return;
 
-    if(this.gameState.board[box.dataset.square] == '') {
-      this.gameState.board[box.dataset.square] = this.gameState.turn;
-      box.innerText = this.gameState.turn;
+    if(this.state.board[box.dataset.square] == '') {
+      this.state.board[box.dataset.square] = this.state.turn;
+      box.innerText = this.state.turn;
       
-      this.gameState.turn = this.gameState.turn == 'X' ? 'O' : 'X';
+      this.state.turn = this.state.turn == 'X' ? 'O' : 'X';
       
-      if(this.gameState.turn == 'X'){
+      if(this.state.turn == 'X'){
         this.setState({winnerLine:'Your Turn...!'});
       }else{
         this.setState({winnerLine:'AI is playing...!'})
       }
-      this.gameState.totalMoves++;
+      this.state.totalMoves++;
     }
 
 
     var result = this.checkWinner();
 
     if(result == 'X') {
-      this.gameState.gameEnded = true;
+      this.state.gameEnded = true;
       this.setState({
         winner: 'X',
         winnerLine: 'X wins the game!'
       });
     } else if(result == 'O') {
-      this.gameState.gameEnded = true;
+      this.state.gameEnded = true;
       this.setState({
         winner: 'O',
         winnerLine: 'O wins the game!'
       });
     } else if(result == 'draw') {
-      this.gameState.gameEnded = true;
+      this.state.gameEnded = true;
       this.setState({
         winner: 'draw',
         winnerLine: 'It\'s draw!'
       })
     }
     
-    if(this.gameState.turn == 'O' && !this.gameState.gameEnded) {
-      this.gameState.gameLocked = true;
+    if(this.state.turn == 'O' && !this.state.gameEnded) {
+      this.state.gameLocked = true;
       setTimeout(()=> {
         do {
           var random = Math.floor(Math.random()*9);
-        } while(this.gameState.board[random] != '');
-        this.gameState.gameLocked = false;
+        } while(this.state.board[random] != '');
+        this.state.gameLocked = false;
         this.clicked(document.querySelectorAll('.square')[random]);
       }, 300);
     }
@@ -76,16 +75,28 @@ class App extends Component {
 
   checkWinner = () => {
     var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
-    var board = this.gameState.board;
+    var board = this.state.board;
     for(let i=0;i<moves.length;i++) {
       if(board[moves[i][0]] == board[moves[i][1]] && board[moves[i][1]] == board[moves[i][2]])
           return board[moves[i][0]];
     }
 
-    console.log(this.gameState.totalMoves);
-    if(this.gameState.totalMoves == 9) {
+    console.log(this.state.totalMoves);
+    if(this.state.totalMoves == 9) {
       return 'draw';
     }
+  }
+
+  playAgain = () => {
+    this.setState({
+      winnerLine:'Let\'s play...!',
+      winner: undefined,
+      turn: 'X',
+      gameLocked: false,
+      gameEnded: false,
+      board: Array(9).fill(''),
+      totalMoves: 0
+    });
   }
 
   render() {
@@ -106,7 +117,7 @@ class App extends Component {
               <div className="square" data-square="8"></div>
           </div>
           <div id="status">{this.state.winnerLine}</div>
-          {this.gameState.gameEnded ? <button className="play-btn">Play Again</button> : ''}
+          {this.state.gameEnded ? <button className="play-btn" onClick={ () => this.playAgain() }>Play Again</button> : ''}
       </div>      
     );
   }
