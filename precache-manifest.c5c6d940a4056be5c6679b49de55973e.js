925188383f551a3be5a19d1acbac99bbc2fb77ff
nishanthdev <nishanthbht@gmail.com>
2018-11-24 14:56:21 +0530

    Updates

---
+++ precache-manifest.c5c6d940a4056be5c6679b49de55973e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe3c286524cc5894d260",
+    "url": "/tictactoe-react/static/js/runtime~main.fe3c2865.js"
+  },
+  {
+    "revision": "b59e83ae8e1e3caa36ac",
+    "url": "/tictactoe-react/static/js/main.b59e83ae.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/tictactoe-react/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "b59e83ae8e1e3caa36ac",
+    "url": "/tictactoe-react/static/css/main.0726fda3.chunk.css"
+  },
+  {
+    "revision": "95d3a6df1964023be8da40b9710f3300",
+    "url": "/tictactoe-react/index.html"
+  }
+];
