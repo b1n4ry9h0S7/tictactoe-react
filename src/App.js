5bab68b8a467159007e4efb838e464dccc955a5f
nishanthdev <nishanthbht@gmail.com>
2018-11-24 16:42:12 +0530

    Pre-Ai implementation branch

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
       board: Array(9).fill(''),
       totalMoves: 0
     };
+    var winMoves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
 
   }
 
